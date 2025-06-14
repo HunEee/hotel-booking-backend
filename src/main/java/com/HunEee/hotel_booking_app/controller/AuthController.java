da0fe5616022880e39448901007463a30b53b12d
HunEee <asdf981001@naver.com>
2025-06-14 12:34:54 +0900

    login 맵핑 완성

--- src/main/java/com/HunEee/hotel_booking_app/controller/AuthController.java
+++ src/main/java/com/HunEee/hotel_booking_app/controller/AuthController.java
@@ -16,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.HunEee.hotel_booking_app.exception.UserAlreadyExistsException;
 import com.HunEee.hotel_booking_app.model.User;
+import com.HunEee.hotel_booking_app.request.LoginRequest;
+import com.HunEee.hotel_booking_app.response.JwtResponse;
+import com.HunEee.hotel_booking_app.security.jwt.JwtUtils;
+import com.HunEee.hotel_booking_app.security.user.HotelUserDetails;
 import com.HunEee.hotel_booking_app.service.IUserService;
 
 import jakarta.validation.Valid;
@@ -28,7 +32,7 @@ public class AuthController {
 
     private final IUserService userService;
     private final AuthenticationManager authenticationManager;
-//    private final JwtUtils jwtUtils;
+    private final JwtUtils jwtUtils;
     
     @PostMapping("/register-user")
     public ResponseEntity<?> registerUser(@RequestBody User user){
@@ -41,23 +45,23 @@ public class AuthController {
         }
     }
 
-//    @PostMapping("/login")
-//    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest request){
-//        Authentication authentication =
-//                authenticationManager
-//                        .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//        String jwt = jwtUtils.generateJwtTokenForUser(authentication);
-//        HotelUserDetails userDetails = (HotelUserDetails) authentication.getPrincipal();
-//        List<String> roles = userDetails.getAuthorities()
-//                .stream()
-//                .map(GrantedAuthority::getAuthority).toList();
-//        return ResponseEntity.ok(new JwtResponse(
-//                userDetails.getId(),
-//                userDetails.getEmail(),
-//                jwt,
-//                roles));
-//    }
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest request){
+        Authentication authentication =
+                authenticationManager
+                        .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtTokenForUser(authentication);
+        HotelUserDetails userDetails = (HotelUserDetails) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority).toList();
+        return ResponseEntity.ok(new JwtResponse(
+                userDetails.getId(),
+                userDetails.getEmail(),
+                jwt,
+                roles));
+    }
     
 	
 }
---
+++ src/main/java/com/HunEee/hotel_booking_app/request/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.HunEee.hotel_booking_app.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
